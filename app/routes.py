13d44c0d5f2d4b99791af73a042b31d0bbd31d23
Isaac Lim <r1522660@mvrht.net>
2018-09-14 21:05:35 +0800

    added actual content

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 # Flask
 from flask import render_template, flash, redirect, url_for, request, abort
+from app import app
+
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template("index.html")
+
+
+@app.route('/send')
+def send():
+    return render_template("form.html")
+
+
+@app.route('/share')
+def share():
+    return render_template("share.html")

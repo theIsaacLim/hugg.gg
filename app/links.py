2b329874b34cc086f926f47baf3c1d58d95199da
Isaac Lim <r1522660@mvrht.net>
2018-09-18 21:23:32 +0800

    helpful logs for debugging, site is up for beta and circling around my friends rn
    more helpful logs and minor changes

--- app/links.py
+++ app/links.py
@@ -1,11 +1,9 @@
-urls = [{'num': 0}]
+urls = [{'num': 0}, {'num': 1, 'message': 'seriiiii'}, {'num': 2, 'message': 'Thank you :3 '}, {'num': 3, 'message': 'Nice'}, {'num': 4, 'message': ''}, {'num': 5, 'message': 'A hug'}]
+
 
 
 def generate_newrl():
     global urls
-    for url in urls:
-        if url['num'] != urls.index(url):
-            return urls.index(url)
     return urls[-1]['num'] + 1
 
 
@@ -14,6 +12,7 @@ def add_newrl(message=None):
     num = generate_newrl()
     urls.append({'num': num,
                  'message': message})
+    print(urls)
     return num
 
 

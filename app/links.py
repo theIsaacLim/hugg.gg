b945c092d9a24c1f1359c298d3b894641ce00cfe
Isaac Lim <r1522660@mvrht.net>
2018-09-19 21:10:25 +0800

    google ads, coming soon

--- app/links.py
+++ app/links.py
@@ -1,7 +1,6 @@
 urls = [{'num': 0}, {'num': 1, 'message': 'seriiiii'}, {'num': 2, 'message': 'Thank you :3 '}, {'num': 3, 'message': 'Nice'}, {'num': 4, 'message': ''}, {'num': 5, 'message': 'A hug'}]
 
 
-
 def generate_newrl():
     global urls
     return urls[-1]['num'] + 1
